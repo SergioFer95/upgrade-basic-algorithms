4a7697790067ca0e702a4065bfdbf99504f7da55
Sergio Fernández <sergio.fernndez@bootcamp-upgrade.com>
2021-11-23 11:08:21 +0100

    No important

--- basic-algorithms.js
+++ basic-algorithms.js
@@ -101,20 +101,20 @@ console.log(x4);
 const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
 
 // 1.2 Cambia el primer elemento de avengers a "IRONMAN"
-avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+// avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
 
 // 1.3 Alert numero de elementos en el array usando la propiedad correcta de Array.
-avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+// avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
 
 // 1.4 Añade 2 elementos al array: "Morty" y "Summer". 
 // Muestra en consola el último personaje del array
-const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+// const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
 
 // 1.5 Elimina el último elemento del array y muestra el primero y el último por consola.
-rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+// rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
 
 // 1.6 Elimina el segundo elemento del array y muestra el array por consola.
-rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+// rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
 
 
 
